ca74b37fafc5d98dc69a840a80b2eb8208ea6310
Poeple2112 <ntuanthanh205@gmail.com>
2024-09-08 01:22:59 +0700

    bo sung ttep

--- asset/js/main.js
+++ asset/js/main.js
@@ -2,14 +2,23 @@ const darkmode_change = document.querySelector(".setting_btn");
 const nav_bar = document.querySelector(".left_side");
 const right_ct = document.querySelector(".right_side");
 // const navicon = document.querySelector("#nav");
-let dark_light = true;
+let dark_light = false;
 let check_nav = true;
 function darkmode() {
+    console.log(dark_light)
+
     if(dark_light){
-        right_ct.style.backgroundColor = ' rgba(0, 0, 0, 0.826);'
+        right_ct.style.backgroundColor = ' rgba(0, 0, 0, 0.826)';
+        darkmode_change.innerHTML = `<i class="fa-regular fa-moon"></i> Dark`;
+        darkmode_change.style.backgroundColor = "white";
+         darkmode_change.style.color = "black";
         dark_light = false;
+        
     }else{
-        right_ct.style.backgroundColor = 'white'
+        right_ct.style.backgroundColor = 'rgb(255, 255, 255)';
+        darkmode_change.style.backgroundColor = "black";
+        darkmode_change.style.color = "white";
+        darkmode_change.innerHTML = `<i class="fa-regular fa-sun"></i> Light`;
         dark_light = true;
     }
 }
@@ -27,3 +36,23 @@ function bar() {
     check_nav = true;
   }
 }
+const logos = ['<i class="fa-brands fa-css3-alt"></i>', '<i class="fa-brands fa-html5"></i>','<i class="fa-brands fa-js"></i>']; // Danh sách các hình ảnh logo
+const logoh1 = document.getElementById('logoh1');
+let currentLogoIndex = 0;
+
+// Hàm thay đổi logo
+function changeLogo() {
+    
+    logoh1.style.opacity = 0; // Ẩn logo hiện tại
+    logoh1.style.transform = `translateX(0px)` 
+    setTimeout(function() {
+        currentLogoIndex = (currentLogoIndex + 1) % logos.length; // Chuyển sang logo tiếp theo
+        console.log(currentLogoIndex)
+        logoh1.innerHTML = `${logos[currentLogoIndex]}`; // Đặt hình ảnh mới
+        logoh1.style.opacity = 1;
+        logoh1.style.transform = `translateX(-60px)` 
+    }, 500); // Thời gian chuyển đổi 0.5 giây
+}
+
+// Gọi hàm thay đổi logo mỗi 2 giây
+setInterval(changeLogo, 2000); // Chuyển đổi mỗi 2 giây
